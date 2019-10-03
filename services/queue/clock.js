b3ee10711a72b8ffbfa92d2c86500439afc98dc6
Olanipekun Ifeoluwa Olayinka <olanipekunifeoluwao@gmail.com>
2019-10-03 03:37:15 +0100

    Update clock.js

--- services/queue/clock.js
+++ services/queue/clock.js
@@ -1,47 +1,16 @@
 "use strict";
 
-var CronJob = require('cron').CronJob;
 var log = require('../logger');
 var Model = require('./Model');
 var queue = require('./');
-var _ = require('lodash');
-var config = require('../../config');
 
 log.info('Starting Queue Clock...');
 Model.find({enabled: true})
 .then(function(jobs){
-    _.forEach(jobs, function(job){
+    jobs.map(job=>{
         log.info('Initializing '+job.name+'...');
-        var cron = new CronJob({
-            cronTime: job.crontab,
-            onTick: function(){
-                // run this
-                // Check if job is enabled
-                Model.findOne({_id: job._id, enabled: true})
-                .then(function(resp){
-                    if(!resp){
-                        throw {notEnabled: true};
-                    }else{
-                        log.info('Pushing '+job.name+' to queue...');
-                        queue.create(job.job, job.arguments)
-                        .save();
-                          resp.lastRunAt = new Date()
-                        return resp.save()
-                    }
-                })
-                .catch(function(err){
-                    if(err.notEnabled){
-                        log.info(job.name+' is not enabled. Skipping...');
-                    }else{
-                        log.error('An error occured while running Job - '+job.name, err);
-                    }
-                });
-
-            },
-            start: true,
-            timeZone: config.clockTimezone
-        });
-    });
+        queue.add(job.job, job.arguments, { repeat: { cron: job.crontab} });
+    })
 })
 .catch(function(err){
     log.error('An error occured while starting the queue clock: ', err);
