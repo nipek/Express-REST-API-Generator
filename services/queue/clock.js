3dc61f9fe88e5fbe3a57c9c8f412d77b55031534
Nipek <olanipekunifeoluwao@gmail.com>
2020-05-21 17:39:36 +0100

    Merge branch 'master' of https://github.com/iolufemi/Express-REST-API-Generator

--- services/queue/clock.js
+++ services/queue/clock.js
@@ -16,23 +16,4 @@ Model.find({ enabled: true })
         log.error('An error occured while starting the queue clock: ', err);
     });
 
-if (config.cleanUpFailedJobs === 'yes') {
-    var jobCleanUpCron = new CronJob({
-        cronTime: '*/5 * * * *',
-        onTick: function () {
-            queue.kue.Job.rangeByState('failed', 0, 100, 'asc', function (err, jobs) {
-                jobs.forEach(function (job) {
-                    job.remove(function (err) {
-                        if (err) {
-                            throw err;
-                        } else {
-                            log.info('removed failed job #%d', job.id);
-                        }
-                    });
-                });
-            });
-        },
-        start: true,
-        timeZone: config.clockTimezone
-    });
-}
+
